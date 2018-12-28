38d717bbc6146852e49449238448f56c43fa96a6
guenther <email@suepergue.de>
2018-12-28 11:19:55 +0100

    adds function to clean up raw data

---
+++ src/purifyData.js
@@ -0,0 +1,27 @@
+import * as R from 'ramda';
+
+const modifyMessageObj = messageObj => {
+    delete messageObj.mentions;
+    delete messageObj.channels;
+    messageObj.user = messageObj.u.username;
+    delete messageObj.u;
+    delete messageObj._updatedAt;
+    return messageObj;
+};
+
+const purifyData = data => {
+    const myData = R.clone(data);
+    const messagesArray = myData.messages;
+    return new Promise((resolve, reject) => {
+        try {
+            const cleanMessagesArray = messagesArray.map(item => {
+                return modifyMessageObj(item);
+            });
+            resolve(cleanMessagesArray);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+export default purifyData;
