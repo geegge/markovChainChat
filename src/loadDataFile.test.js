d206cf7cb34045e4fbbce1c9b40ea66b87c2f5e9
guenther <email@suepergue.de>
2018-12-28 11:50:58 +0100

    adds test in case no file found

--- src/loadDataFile.test.js
+++ src/loadDataFile.test.js
@@ -2,11 +2,16 @@ import loadDataFile from './loadDataFile';
 const path = require('path');
 
 describe('testing file loading', () => {
-    test('load content from filet', async () => {
+    it('should load content from filet', async () => {
         expect.assertions(1);
         const data = await loadDataFile(
             path.resolve(__dirname, './loadDataFiletest.txt')
         );
         expect(data).toBe('lorem ipsum');
+    });
+
+    it('should throw error without file', async () => {
+        expect.assertions(1);
+        await expect(loadDataFile('./nonExistingFile.txt')).rejects.toThrow();
     });
 });
