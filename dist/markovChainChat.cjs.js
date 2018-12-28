c16e622b182a0b67d5513ac40b8c03fb6955a801
guenther <email@suepergue.de>
2018-12-28 11:18:07 +0100

    newly built files

--- dist/markovChainChat.cjs.js
+++ dist/markovChainChat.cjs.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var R = require('ramda');
+
 const fs = require('fs');
 
 const loadDataFile = file => {
@@ -11,14 +13,55 @@ const loadDataFile = file => {
     });
 };
 
+const prepareData = data => {
+    const myData = R.clone(data);
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(JSON.parse(myData));
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const modifyMessageObj = messageObj => {
+    delete messageObj.mentions;
+    delete messageObj.channels;
+    messageObj.user = messageObj.u.username;
+    delete messageObj.u;
+    delete messageObj._updatedAt;
+    return messageObj;
+};
+
+const purifyData = data => {
+    const myData = R.clone(data);
+    const messagesArray = myData.messages;
+    return new Promise((resolve, reject) => {
+        try {
+            const cleanMessagesArray = messagesArray.map(item => {
+                return modifyMessageObj(item);
+            });
+            resolve(cleanMessagesArray);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
 class markovChainChat {
     constructor(textFile) {
-        this.textData = this.readAndProcess(textFile);
+        this.readProcessStore(textFile);
     }
-    async readAndProcess(filePath) {
-        let rawData = await loadDataFile(filePath);
-        console.log('[[markovChainChat]] data: ', rawData);
-        //let result  = await processData( content )
+    async readProcessStore(filePath) {
+        //@todo: make it work fp
+        const rawData = await loadDataFile(filePath);
+        const refinedData = await prepareData(rawData);
+        const messagesArr = await purifyData(refinedData);
+        //@todo: function for storing data
+        console.log(
+            '[[markovChainChat]] messagesArr: ',
+            messagesArr[messagesArr.length - 1]
+        );
     }
 }
 
