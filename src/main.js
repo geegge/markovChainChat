8c4bf8b5e17c201fae6f38cd666aa9abbb2595fd
guenther <email@suepergue.de>
2019-01-23 00:27:32 +0100

    adds very basic draft how to use matrices here

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda';
+import * as math from 'mathjs';
 import loadDataFile from './utility/loadDataFile.js';
 import prepareData from './utility/prepareData.js';
 import purifyData from './utility/purifyData.js';
@@ -18,13 +19,30 @@ class markovChainChat {
         );
         const myfineData = getcleansedData(rawData);
 
-        const MyUniqueContentList = getUniqueMessageContent(myfineData);
+        const myUniqueContentList = getUniqueMessageContent(myfineData);
+
+        //draft!!!
+        //adding probabilites from readme for testing (left direction in matrice is down in table)
+        const m1 = math.matrix([
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0.5, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0.5, 0]
+        ]);
+
+        let msgIndex = null;
+        myUniqueContentList.forEach((ele, index) => {
+            if (ele.msg === 'Hallo') {
+                msgIndex = index;
+            }
+        });
+
+        console.log(math.subset(m1, math.index(math.range(0, 4))));
 
         //@todo: function for storing data
-        console.log(
-            '[[markovChainChat]] MyUniqueContentList: ',
-            MyUniqueContentList
-        );
     }
 }
 
